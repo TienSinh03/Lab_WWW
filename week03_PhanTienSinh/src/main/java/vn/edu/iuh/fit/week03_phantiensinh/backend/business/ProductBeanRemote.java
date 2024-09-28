90e54e4df267848f64c1aa99bba6651df8cd825a
TienSinh03 <zatos1232@gmail.com>
2024-09-28 18:54:56 +0700

    add project week03

---
+++ week03_PhanTienSinh/src/main/java/vn/edu/iuh/fit/week03_phantiensinh/backend/business/ProductBeanRemote.java
@@ -0,0 +1,25 @@
+/**
+ * @ (#) ProductBeanRemote.java      9/21/2024
+ * <p>
+ * Copyright (c) 2024 IUH. All rights reserved
+ */
+
+package vn.edu.iuh.fit.week03_phantiensinh.backend.business;
+
+import jakarta.ejb.Remote;
+import vn.edu.iuh.fit.week03_phantiensinh.backend.repositories.entities.Product;
+
+import java.util.List;
+
+/*
+ * @description:
+ * @author: Sinh Phan Tien
+ * @date: 9/21/2024
+ */
+@Remote
+public interface ProductBeanRemote {
+    void add (Product product);
+   List<Product> getAll();
+    Product getById(int id);
+
+}
